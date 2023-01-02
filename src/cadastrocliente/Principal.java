d84d6808f76d7bd77f298964f93124fd23f9ba94
unknown <gustavo.picoli@hotmail.com>
2023-01-02 15:16:59 -0300

    Criação do projeto

---
+++ src/cadastrocliente/Principal.java
@@ -0,0 +1,21 @@
+
+package cadastrocliente;
+
+import Forms.TelaCadastro;
+
+/**
+ *
+ * @author Gustavo Picoli
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaCadastro c = new TelaCadastro();
+        c.setVisible(true);
+    }
+    
+}
